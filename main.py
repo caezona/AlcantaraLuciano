2b57f2a9994c332da4a358f0dce65005533ba51f
Alcantara Luciano <cabezon201205@gmail.com>
2024-06-28 20:06:25 -0400

    Aqui agrege la clasificación de los saldos que seria el valor bajo, el medio y el alto

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def generar_saldos():
     saldo = random.randint(1000, 100000)
     saldos.append(saldo)
   return saldos
+
+# Función para clasificar saldos
+def clasificar_saldos(saldos):
+  saldos_bajos = [saldo for saldo in saldos if saldo <= 20000]
+  saldos_medios = [saldo for saldo in saldos if 20001 <= saldo <= 50000]
+  saldos_altos = [saldo for saldo in saldos if saldo > 50001]
+  print("Saldos bajos:", saldos_bajos)
+  print("Saldos medios:", saldos_medios)
+  print("Saldos altos:", saldos_altos)
